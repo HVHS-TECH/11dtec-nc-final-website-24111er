a8935c25ab03791356882fe7abe1c57ae95b5715
24111er <24111er@hvhs.school.nz>
2026-03-28 01:14:31 +0000

    Mar 28, 2026, 1:14 AM

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const icon = document.getElementById("icon");
 const message = document.getElementById("message");
-const popupN = document.getElementById("NarniaPopup")
+const popupN = document.getElementById("NarniaPopup");
+const close = document.getElementsByClassName("close-button");
 
-function handleClick() {
-    popupN.
+icon.onclick = function() {
+    popupN.style.display = "block";
 }
 
-icon.addEventListener('click', handleClick);
+close.onclick = function() {
+    popupN.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == popupN) {
+        popupN.style.display = "none";
+    }
+}
+
+
+
+// https://www.w3schools.com/howto/howto_css_modals.asp#:~:text=Converters,padding:%2020px;//
