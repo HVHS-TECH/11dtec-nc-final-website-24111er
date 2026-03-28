7e1ae7178ac11c318c8606202fea6fb6bfd37342
24111er <24111er@hvhs.school.nz>
2026-03-28 01:01:51 +0000

    Mar 28, 2026, 1:01 AM

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const icon = document.getElementById("icon");
 const message = document.getElementById("message");
+const popupN = document.getElementById("NarniaPopup")
 
 function handleClick() {
     message.textContent = 'The button was clicked';
