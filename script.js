b56333480f6d702297a74c02547c64ae42d2f534
24111er <24111er@hvhs.school.nz>
2026-03-29 21:35:17 +0000

    Mar 29, 2026, 9:35 PM

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ const message = document.getElementById("message");
 const popupN = document.getElementById("NarniaPopup");
 const close = document.getElementsById("close-button");
 
-icon.onclick = function() {
-//     popupN.style.display = "block";
-message.textContent = "the button was cliked";
- }
+// icon.onclick = function() {
+// //     popupN.style.display = "block";
+// message.textContent = "the button was cliked";
+//  }
+
+function handleClick {
+    message.textContent = "the button was clicked";
+}
+
+ icon.addEventListener('click',handleClick)
 
 // close.onclick = function() {
 //     popupN.style.display = "none";
