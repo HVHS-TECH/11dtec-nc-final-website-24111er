6c3f27e4b88c0e364157e6396864da6b7fc8f648
24111er <24111er@hvhs.school.nz>
2026-04-10 04:04:37 +0000

    Apr 10, 2026, 4:04 AM

--- script.js
+++ script.js
@@ -15,7 +15,12 @@ function NarniaClose() {
 }
 
 
-for (i = 0; i < coll.length)
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        this.classList.toggle("active")
+    
+    });
+}
 // for (i = 0; i < coll.length; i++) {
 //     coll[i].addEventListener('click', Collapsible);
 // }
