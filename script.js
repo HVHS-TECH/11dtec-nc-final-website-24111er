feed51f355716daf117cdf2c236ccea357612a2d
24111er <24111er@hvhs.school.nz>
2026-04-10 04:31:35 +0000

    Apr 10, 2026, 4:31 AM

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ var i;
 
 for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
-        this.classList.toggle
+        this.classList.toggle("active");
+        var content 
     })
 }
 
