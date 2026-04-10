6c05f25ec0699eeec1d41cd758ba663eb74cc119
24111er <24111er@hvhs.school.nz>
2026-04-10 05:43:38 +0000

    Apr 10, 2026, 5:43 AM

--- script.js
+++ script.js
@@ -6,17 +6,19 @@ const npopup = document.getElementById("NarniaPopup")
 var coll = document.getElementsByClassName("collapsible_title");
 var i;
 
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener('click', function() {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.maxheight) {
-            content.style.maxheight = null;
-        } else {
-            content.style.maxheight = content.scrollHeight + "px";
-        }
-    });
-}
+
+document.body.addEventListener
+// for (i = 0; i < coll.length; i++) {
+//     coll[i].addEventListener('click', function() {
+//         this.classList.toggle("active");
+//         var content = this.nextElementSibling;
+//         if (content.style.maxheight) {
+//             content.style.maxheight = null;
+//         } else {
+//             content.style.maxheight = content.scrollHeight + "px";
+//         }
+//     });
+// }
 
 function NarniaClick() {
     npopup.style.display = "grid";
