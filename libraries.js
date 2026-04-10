84ac9044b933e80f8e006f1ca43df4f1ebc3c0c9
24111er <24111er@hvhs.school.nz>
2026-04-10 05:56:18 +0000

    Apr 10, 2026, 5:56 AM

--- libraries.js
+++ libraries.js
@@ -2,6 +2,6 @@ document.body.addEventListener("click", (ev) => {
     const isExpandableTitle = !!ev.target.closest(".expandable_head");
     const expandable = ev.target.closest(".expandable");
 
-    console.log(isExpandableTitle);
+    console.log(expandable);
 
 });
