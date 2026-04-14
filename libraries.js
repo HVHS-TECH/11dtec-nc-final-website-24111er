aeeb12870f8ccf9109d266a33c68324943b19bbc
24111er <24111er@hvhs.school.nz>
2026-04-14 22:21:06 +0000

    Apr 14, 2026, 10:21 PM

--- libraries.js
+++ libraries.js
@@ -11,3 +11,15 @@ document.body.addEventListener("click", (ev) => {
 
 });
 
+document.body.addEventListener("click", (ev) => {
+    const IconButton = !!ev.target.closest(".icon");
+    const  = ev.target.closest(".popup-content");
+    if (!isExpandableTitle) {
+        return;
+    }
+
+    expandable.classList.toggle("expandable-open")
+
+    // console.log(expandable);
+
+});
