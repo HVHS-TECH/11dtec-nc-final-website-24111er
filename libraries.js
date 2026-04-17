b67ed855bfea2f993251f9d654f5da336f3774e9
24111er <24111er@hvhs.school.nz>
2026-04-17 04:49:52 +0000

    Apr 17, 2026, 4:49 AM

--- libraries.js
+++ libraries.js
@@ -1,26 +1,4 @@
-document.body.addEventListener("click", (ev) => {
-    const isExpandableTitle = !!ev.target.closest(".expandable_head");
-    const expandable = ev.target.closest(".expandable");
-    if (!isExpandableTitle) {
-        return;
-    }
 
-    expandable.classList.toggle("expandable-open");
 
-    // console.log(expandable);
 
-});
-
-document.body.addEventListener("click", (ev) => {
-    const isQuicklinksTitle = !!ev.target.closest(".quicklinks_head");
-    const quicklinks = ev.target.closest(".quicklinks");
-    if (!isQuicklinksTitle) {
-        return;
-    }
-
-    quicklinks.classList.toggle("quicklinks-open");
-
-    // console.log(quicklinks);
-
-});
 
