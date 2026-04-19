4dd02d92fc6c740aa4cc46a54ca4414f0e753944
24111er <24111er@hvhs.school.nz>
2026-04-19 06:24:13 +0000

    Apr 19, 2026, 6:24 AM

--- libraries.js
+++ libraries.js
@@ -1,4 +1,6 @@
- document.body.addEventListener("click", (ev) => {
+ 
+// This is the java code for the expandable content on the library pages
+document.body.addEventListener("click", (ev) => {
     const isExpandableTitle = !!ev.target.closest(".expandable_head");
     const expandable = ev.target.closest(".expandable");
     if (!isExpandableTitle) {
@@ -8,6 +10,9 @@
     expandable.classList.toggle("expandable-open");
 
 });
+
+// When an expanadable title is clicked, the closest title to the event (the one that was clicked) 
+// is registered as constant variable isExpandable title
 
 
 
