15f2239515aaffb0e7901636636b17992bf64de2
24111er <24111er@hvhs.school.nz>
2026-04-19 06:34:08 +0000

    Apr 19, 2026, 6:34 AM

--- libraries.js
+++ libraries.js
@@ -12,9 +12,14 @@ document.body.addEventListener("click", (ev) => {
 });
 
 // When an expanadable title is clicked, the closest title to the event (the one that was clicked) 
-// is registered as constant "isExpandable title" and the closest expandable content to the click 
+// is registered as constant "isExpandableTitle" and the closest expandable content to the click 
 // event is registered as the constant "expandable". If the target of the click wasn't a "isExpandableTitle" 
 // (the user clicked the page or another element) than it stops executing the function. Otherwise
+//  the computer continues to execute the code. The "expandable" constant is toggled with the css class 
+// expandable-open" on the class list. This means that if on the class list (the class list is the list of 
+// classes across css and html) the expandable class is there (and this funciton is executed) this will be 
+// toggled (switched in and out) with the "expandable-open class". This makes the expandable content appear 
+// (with the css code addign the transition).
 
 // I used this tutorial to help teach me how to do this (https://www.youtube.com/watch?v=9nJxybp7CEc)
 
